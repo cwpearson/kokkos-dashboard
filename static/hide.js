84a1ed6b048de7536d7141477f7f9661cc84da0a
Carl Pearson <me@carlpearson.net>
2025-09-26 05:43:43 -0600

    Replace javascript with details
    Signed-off-by: Carl Pearson <me@carlpearson.net>

--- static/hide.js
+++
@@ -1,7 +0,0 @@
-function hideIssue(button) {
-    // Find the parent .issue div and hide it
-    const issueDiv = button.closest('.issue');
-    if (issueDiv) {
-        issueDiv.style.display = 'none';
-    }
-}
